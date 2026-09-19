f785ac5a6865d3560ee95e868919fe26db307fad
Thabitha2007 <tthabitha658@gmail.com>
2026-09-20 00:11:34 +0530

    schemma.sql

--- src/main/java/com/thabitha/thabithamart/util/DB.java
+++ src/main/java/com/thabitha/thabithamart/util/DB.java
@@ -11,29 +11,24 @@ public final class DB {
     }
 
     public static Connection get() throws SQLException {
-
+        // H2 Database URL-லேயே schema.sql-ஐத் தானாக ரன் செய்ய INIT கட்டளை சேர்க்கப்பட்டுள்ளது
         return DriverManager.getConnection(
-                "jdbc:h2:~/thabithamart;MODE=PostgreSQL",
+                "jdbc:h2:~/thabithamart;MODE=PostgreSQL;INIT=RUNSCRIPT FROM 'classpath:schema.sql'",
                 "sa",
                 ""
         );
     }
 
     public static void init() {
-
         try (
                 Connection c = get();
                 Statement s = c.createStatement()
         ) {
-
             s.execute(
                     "RUNSCRIPT FROM 'classpath:schema.sql'"
             );
-
         } catch (Exception e) {
-
             throw new RuntimeException(e);
-
         }
     }
 }
